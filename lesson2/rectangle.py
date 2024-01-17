fe930af1acbdab65183575bc006409060f0a6ea2
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-01-17 13:14:34 +0500

    add from_diagonal

--- lesson2/rectangle.py
+++ lesson2/rectangle.py
@@ -12,24 +12,33 @@ from math import sqrt
 
 class Rectangle:
     def __init__(self, width, height):
+        """ конструктор, принимающий ширину и высоту прямоугольника """
         self.width = width
         self.height = height
 
     def area(self):
+        """ метод, возвращающий площадь прямоугольника """
         return self.width * self.height
 
     def perimeter(self):
+        """ метод, возвращающий периметр прямоугольника """
         return (self.width + self.height) * 2
 
     @classmethod
     def from_diagonal(cls, diagonal, aspect_ratio):
-        return cls(diagonal, aspect_ratio)
+        """ класс-метод, принимающий диагональ прямоугольника и соотношение сторон и возвращающий объект класса Rectangle """
+        height = (diagonal ** 2 / (aspect_ratio ** 2 + 1)) ** 0.5
+        width = aspect_ratio * height
+        return cls(height, width)
 
     @staticmethod
     def is_square(width, height):
+        """ статический метод, принимающий ширину и высоту прямоугольника и возвращающий True,
+если это квадрат, и False в противном случае """
         if width == height:
             return True
-        return False
+        else:
+            return False
 
 
 # код для проверки 
