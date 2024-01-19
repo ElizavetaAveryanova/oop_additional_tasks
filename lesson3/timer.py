762636a441908082dba7eeb31e7d97d59682fcc5
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-01-19 14:42:45 +0500

    lesson_3

--- lesson3/timer.py
+++ lesson3/timer.py
@@ -9,14 +9,18 @@ import time
 
 class Timer:
     def __enter__(self):
+        """Магический метод, который запускает таймер"""
         self.start_time = time.time()
 
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Магический метод, который останавливает таймер
+        и выводит время выполнения блока кода"""
+        self.end_time = time.time()
+        elapsed_time = self.end_time - self.start_time
+        print(f'Execution time:{elapsed_time}')
 
 
 with Timer() as timer:
+    time.sleep(7)
 
 
-
-
-# код для проверки 
-print("Execution time:", timer.elapsed_time)
